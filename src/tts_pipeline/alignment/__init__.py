2d9156356fde4513b0de57141fd56bf86b70324f
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-07-18 17:46:53 +0530

    feat: add tts_pipeline/alignment/__init__.py

---
+++ src/tts_pipeline/alignment/__init__.py
@@ -0,0 +1,5 @@
+"""Duration alignment interfaces and artifact management."""
+
+from tts_pipeline.alignment.duration import AlignmentArtifact, UniformAlignmentBackend
+
+__all__ = ["AlignmentArtifact", "UniformAlignmentBackend"]
