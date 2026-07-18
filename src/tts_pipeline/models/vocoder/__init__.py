f6503f0f64ae429a5e1f8a4c3da4fe50301ec8ab
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-07-18 17:46:53 +0530

    feat: add tts_pipeline/models/vocoder/__init__.py

---
+++ src/tts_pipeline/models/vocoder/__init__.py
@@ -0,0 +1,7 @@
+from tts_pipeline.models.vocoder.hifigan import (
+    HiFiGANGenerator,
+    MultiPeriodDiscriminator,
+    MultiScaleDiscriminator,
+)
+
+__all__ = ["HiFiGANGenerator", "MultiPeriodDiscriminator", "MultiScaleDiscriminator"]
